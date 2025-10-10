6e0bfd1116ec165e7a5de9eeff8db15264ff8416
Masudur400 <md8004210@gmail.com>
2025-10-10 14:30:31 +0600

    text size fixed

--- src/components/HomeComponent/Banner.tsx
+++ src/components/HomeComponent/Banner.tsx
@@ -20,11 +20,11 @@ const Banner = () => {
                                 className="h-72 w-72 lg:h-96 lg:w-96 border-4 border-white rounded-full lg:ml-20 md:ml-10 md:mx-1 mx-auto md:my-8 animate-float"
                             />
 
-                            <div className='text-white text-opacity-80 opacity-90 max-sm:ml-4 lg:w-3/6 md:w-2/4 mt-3 flex flex-col gap-5'>
+                            <div className='text-white text-opacity-80 opacity-90 max-sm:px-2 md:px-2 lg:w-3/6 md:w-2/4 mt-3 flex flex-col gap-5'>
                                 <div>
                                     <p className='lg:text-3xl md:text-2xl text-xl font-bold'>Hi !</p>
                                     <div className="flex">
-                                        <p className='lg:text-4xl md:text-3xl text-2xl font-bold lg:ml-12 md:ml-12 mb-2'>
+                                        <p className='lg:text-3xl xl:text-4xl md:text-2xl text-2xl font-bold lg:ml-12 md:ml-12 mb-2'>
                                             I am{" "}
                                         </p>
                                         <TextType
@@ -35,7 +35,7 @@ const Banner = () => {
                                             variableSpeed={{ min: 20, max: 50 }}
                                             showCursor={true}
                                             cursorCharacter=""
-                                            className="lg:text-4xl md:text-3xl text-2xl font-bold ml-2 mb-2"
+                                            className="lg:text-3xl xl:text-4xl md:text-2xl text-2xl font-bold ml-2 mb-2"
                                         />
                                     </div>
                                 </div>
